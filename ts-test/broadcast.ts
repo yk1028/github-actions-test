bb5a0df6f9aea797a8ed6c0ed43d9c25c9140d22
wongeunsong <wongeunsong94@gmail.com>
2023-12-20 19:25:30 +0900

    feat: update

--- ts-test/broadcast.ts
+++ ts-test/broadcast.ts
@@ -17,32 +17,32 @@ const store = async () => {
     console.log(wasmPath)
     console.log(adminMnemonic)
 
-    // const cube_wallet01 = cube.wallet(new MnemonicKey({mnemonic: adminMnemonic}))
+    const cube_wallet01 = cube.wallet(new MnemonicKey({mnemonic: adminMnemonic}))
 
-    // const storeCode = new MsgStoreCode(
-    //     cube_wallet01.key.accAddress,
-    //     fs.readFileSync(path.resolve(__dirname, wasmPath), 'base64')
-    // )
+    const storeCode = new MsgStoreCode(
+        cube_wallet01.key.accAddress,
+        fs.readFileSync(path.resolve(__dirname, wasmPath), 'base64')
+    )
 
-    // const storeCodeTx = await cube_wallet01.createAndSignTx({
-    //     msgs: [storeCode],
-    // });
+    const storeCodeTx = await cube_wallet01.createAndSignTx({
+        msgs: [storeCode],
+    });
 
-    // const storeCodeTxResult = await cube.tx.broadcast(storeCodeTx);
+    const storeCodeTxResult = await cube.tx.broadcast(storeCodeTx);
 
-    // console.log(storeCodeTxResult);
+    console.log(storeCodeTxResult);
 
-    // if (isTxError(storeCodeTxResult)) {
-    //     throw new Error(
-    //         `store code failed. code: ${storeCodeTxResult.code}, codespace: ${storeCodeTxResult.codespace}, raw_log: ${storeCodeTxResult.raw_log}`
-    //     );
-    // }
+    if (isTxError(storeCodeTxResult)) {
+        throw new Error(
+            `store code failed. code: ${storeCodeTxResult.code}, codespace: ${storeCodeTxResult.codespace}, raw_log: ${storeCodeTxResult.raw_log}`
+        );
+    }
 
-    // const {
-    //     store_code: { code_id },
-    // } = storeCodeTxResult.logs[0].eventsByType;
+    const {
+        store_code: { code_id },
+    } = storeCodeTxResult.logs[0].eventsByType;
 
-    // console.log(code_id)
+    console.log(code_id)
 }
 
 store();
